3d24e6e5276d965c33a92899fa3b2d27d6facf24
Anshul Yadav <anshuly503@gmail.com>
2026-06-04 15:14:40 +0530

    Implement diagonal sum calculation for a matrix

---
+++ Week_1/Day_4/matrix_diagonal_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for(int i = 0 ; i<n ; i++)
+        {
+            sum = sum + mat[i][i];
+            sum = sum + mat[i][n-1-i];
+            if (i == n - 1 - i) {
+                sum =sum - mat[i][i];
+            }
+        }
+        return sum;
+    }
+};
