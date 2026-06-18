d8e944d0b45e01dbc6f874c528fb62c3f07c02a4
Anshul Yadav <anshuly503@gmail.com>
2026-06-18 17:12:58 +0530

    Implement longest palindrome substring finder

---
+++ Week_2/Day_5/Longest_pallindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int left, int right) {
+        while (left <right){
+            if (s[left] !=s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        int start =0;
+        for (int i =0; i< n;i++){
+            for (int j =i;j <n; j++){
+                if (isPalindrome(s,i,j)){
+                    int len=j -i+ 1;
+                    if (len>maxLen) {
+                        maxLen=len;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxLen);
+    }
+};
